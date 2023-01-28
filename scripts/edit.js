025ba6d2a93bbffcf11b7a5cec85cfc44dce32fc
Daniel Fehlow <daniel.santos_59@hotmail.com>
2023-01-28 14:09:19 -0300

    Ajuste conforme PR

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,31 +1,28 @@
-let getLocalStorage = (key) => JSON.parse(localStorage.getItem(key))
-let setLocalStorage = (key, item) =>
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key))
+const setLocalStorage = (key, item) =>
   localStorage.setItem(key, JSON.stringify(item))
-database = getLocalStorage('database')
-index = getLocalStorage('index')
-let form = document.getElementById('form')
+const database = getLocalStorage('database')
+const index = getLocalStorage('index')
+const form = document.getElementById('form')
 const checkFields = () => {
   return form.reportValidity()
 }
-let updadepost = document.getElementById('button')
-updadepost.addEventListener('click', edit)
-document.getElementById('texttitle').value = database[index].title
+document.getElementById('text-title').value = database[index].title
 document.getElementById('name').value = database[index].name
 document.getElementById('text').value = database[index].text
 document.getElementById('urlimage').value = database[index].urlimage
 
-function edit() {
+const edit = () => {
   if (!checkFields()) {
     return null
   }
-  index = getLocalStorage('index')
-  database = getLocalStorage('database')
+  const index = getLocalStorage('index')
+  const database = getLocalStorage('database')
 
-  //fillfields(database)
-  let name = document.getElementById('name')
-  let text = document.getElementById('text')
-  let title = document.getElementById('texttitle')
-  let urlimage = document.getElementById('urlimage')
+  const name = document.getElementById('name')
+  const text = document.getElementById('text')
+  const title = document.getElementById('text-title')
+  const urlimage = document.getElementById('urlimage')
   date = new Date()
   let meses = [
     'Jan',
@@ -42,17 +39,19 @@ function edit() {
     'Dez',
   ]
   datea = `${date.getDate()} ${meses[date.getMonth()]} ${date.getFullYear()}`
-
-  let newdatabase = {
+  
+  const newdatabase = {
     title: title.value,
     name: name.value,
     text: text.value,
     urlimage: urlimage.value,
     date: datea,
   }
-  //quando coloco um novo valor no array, o array original não muda
+  
   database[index] = newdatabase
   localStorage.setItem('database', JSON.stringify(database))
   url = 'post.html'
   window.open(url, '_self')
 }
+updadepost = document.getElementById('button')
+updadepost.addEventListener('click', edit)
